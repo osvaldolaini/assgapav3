eecf1d513469cae95d00f472b35556e61d43c06c
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-01-02 15:04:16 -0300

    master

--- app/Livewire/Admin/Ambiences/Ambiences.php
+++ app/Livewire/Admin/Ambiences/Ambiences.php
@@ -267,7 +267,7 @@ class Ambiences extends Component
             $query = $this->model::query();
         } else {
             $query = $this->model::query();
-            $query = $query->where('active', '<=', 1);
+            $query = $query->where('ambiences.active', '<=', 1);
         }
         $selects = array($this->modelId . ' as id');
         if ($this->columnsInclude) {
