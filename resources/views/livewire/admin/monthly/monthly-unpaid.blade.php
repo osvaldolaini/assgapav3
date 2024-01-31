1b9b3b6311833889f7c2629e3f2c85d5505af513
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-01-31 20:00:54 -0300

    master

--- resources/views/livewire/admin/monthly/monthly-unpaid.blade.php
+++ resources/views/livewire/admin/monthly/monthly-unpaid.blade.php
@@ -203,7 +203,7 @@
                         font-medium rounded-lg text-sm px-5 py-2.5
                         text-center dark:bg-blue-600 dark:hover:bg-blue-700
                         dark:focus:ring-blue-800">
-                Pagar
+                Atualizar
             </button>
             <x-secondary-button wire:click="$toggle('showModalEdit')" class="mx-2">
                 Fechar
