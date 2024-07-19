d02c4cc69bbb6691a400b24c035d2102ac74d030
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-07-19 05:21:48 -0300

    master

--- resources/views/livewire/admin/locations/location-contract.blade.php
+++ resources/views/livewire/admin/locations/location-contract.blade.php
@@ -218,8 +218,8 @@
                         <td colspan="2">Funcionamento dias de semana:</td>
                         <td colspan="3">{{ $location->ambiences->time_week }}</td>
                     <tr>
-                        <td>Dia do evento: {{ $location->location_date }}</td>
-                        <td colspan="4">Valor da locação: R$ {{ $location->value }}</td>
+                        <td colspan="2">Dia do evento: {{ $location->location_date }}</td>
+                        <td colspan="3">Valor da locação: R$ {{ $location->value }}</td>
                         {{-- <td colspan="2">Valor caução: R$ {{ $location->deposit }}</td> --}}
                     </tr>
 
