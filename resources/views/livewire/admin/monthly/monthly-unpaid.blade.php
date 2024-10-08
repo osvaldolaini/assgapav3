bfcb250dd942192c24ea972926dd9667090e3d3c
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-10-08 08:16:59 -0300

    colocado 0 antes do digitos nos meses para padronizar

--- resources/views/livewire/admin/monthly/monthly-unpaid.blade.php
+++ resources/views/livewire/admin/monthly/monthly-unpaid.blade.php
@@ -1,32 +1,32 @@
 <div
-    class="flex flex-col items-center justify-between px-4 mb-5
-                space-y-3 md:flex-row md:space-y-0 md:space-x-4">
-    <div class="w-full py-2 px-4 shadow-md bg-red-200 rounded-t-md">
+    class="flex flex-col items-center justify-between px-4 mb-5 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+    <div class="w-full px-4 py-2 bg-red-200 shadow-md rounded-t-md">
         <div class="grid grid-cols-8 gap-4 text-gray-600 ">
             <div class="col-span-6 justify-items-start">
-                <h3 class="mb-5 text-2xl font-bold tracki text-red-500">Não pagas</h3>
+                <h3 class="mb-5 text-2xl font-bold text-red-500 tracki">Não pagas</h3>
             </div>
-            <div class="col-span-2 flex justify-end space-x-2">
-                <button wire:click="modalCreate()" class="btn btn-info text-white">Criar
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="currentColor" xviewBox="0 0 24 24">
+            <div class="flex justify-end col-span-2 space-x-2">
+                <button wire:click="modalCreate()" class="text-white btn btn-info">Criar
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 " fill="currentColor" xviewBox="0 0 24 24">
                         <path
                             d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1.41 16.09V20h-2.67v-1.93c-1.71-.36-3.16-1.46-3.27-3.4h1.96c.1 1.05.82 1.87 2.65 1.87 1.96 0 2.4-.98 2.4-1.59 0-.83-.44-1.61-2.67-2.14-2.48-.6-4.18-1.62-4.18-3.67 0-1.72 1.39-2.84 3.11-3.21V4h2.67v1.95c1.86.45 2.79 1.86 2.85 3.39H14.3c-.05-1.11-.64-1.87-2.22-1.87-1.5 0-2.4.68-2.4 1.64 0 .84.65 1.39 2.67 1.91s4.18 1.39 4.18 3.91c-.01 1.83-1.38 2.83-3.12 3.16z" />
                     </svg>
                 </button>
-                <button wire:click="paid()" class="btn btn-success text-white">Pagar
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="currentColor" xviewBox="0 0 24 24">
+                <button wire:click="paid()" class="text-white btn btn-success">Pagar
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 " fill="currentColor" xviewBox="0 0 24 24">
                         <path
                             d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1.41 16.09V20h-2.67v-1.93c-1.71-.36-3.16-1.46-3.27-3.4h1.96c.1 1.05.82 1.87 2.65 1.87 1.96 0 2.4-.98 2.4-1.59 0-.83-.44-1.61-2.67-2.14-2.48-.6-4.18-1.62-4.18-3.67 0-1.72 1.39-2.84 3.11-3.21V4h2.67v1.95c1.86.45 2.79 1.86 2.85 3.39H14.3c-.05-1.11-.64-1.87-2.22-1.87-1.5 0-2.4.68-2.4 1.64 0 .84.65 1.39 2.67 1.91s4.18 1.39 4.18 3.91c-.01 1.83-1.38 2.83-3.12 3.16z" />
                     </svg>
                 </button>
             </div>
         </div>
-        <div class="space-y-4 w-full mt-2">
-            <div class="grid grid-cols-3 gap-1 w-full">
+        <div class="w-full mt-2 space-y-4">
+            <div class="grid w-full grid-cols-3 gap-1">
                 @foreach ($monthlys as $item)
-                    <div class="indicator w-full">
+                    <div class="w-full indicator">
                         <span class="indicator-item indicator-center">
-                            <span class="badge badge-primary cursor-pointer" wire:click="showModalUpdate({{ $item->id }})">
+                            <span class="cursor-pointer badge badge-primary"
+                                wire:click="showModalUpdate({{ $item->id }})">
                                 <svg class="w-4 h-4 mr-1 text-white" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -36,28 +36,24 @@
                                 </svg>
                                 <span class="">Editar</span>
                             </span>
-                            <span class="badge badge-error cursor-pointer" wire:click="showModalDelete({{ $item->id }})"">
+                            <span class="cursor-pointer badge badge-error"
+                                wire:click="showModalDelete({{ $item->id }})"">
                                 <svg class="w-4 h-4 mr-1 text-white" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                                </path>
-                            </svg>
+                                    stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                                    </path>
+                                </svg>
                                 <span class="text-white">Excluir</span>
                             </span>
 
                         </span>
-                        <div class="flex  w-full">
+                        <div class="flex w-full">
                             <input type="checkbox" wire:model="pay" id="monthly-{{ $item->id }}-s"
                                 value="{{ $item->id }}" class="hidden peer" required="">
                             <label for="monthly-{{ $item->id }}-s"
-                                class="inline-flex peer-checked:hidden items-center
-                                justify-between w-full text-gray-500 bg-gray-200 border-2
-                                border-gray-200 rounded-lg cursor-pointer peer-checked:bg-blue-500
-                                 peer-checked:border-blue-500 hover:text-gray-600
-                                 peer-checked:text-gray-600 hover:bg-blue-500
-                                 ">
-                                <div class="flex h-full items-center justify-center p-0 m-0 ">
+                                class="inline-flex items-center justify-between w-full text-gray-500 bg-gray-200 border-2 border-gray-200 rounded-lg cursor-pointer peer-checked:hidden peer-checked:bg-blue-500 peer-checked:border-blue-500 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-blue-500 ">
+                                <div class="flex items-center justify-center h-full p-0 m-0 ">
                                     <svg class="w-12 h-12 p-2 " viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd"
@@ -71,13 +67,8 @@
                                 </div>
                             </label>
                             <label for="monthly-{{ $item->id }}-s"
-                                class="hidden peer-checked:inline-flex items-center
-                                        justify-between w-full text-gray-500 bg-gray-200 border-2
-                                    border-gray-200 rounded-lg cursor-pointer peer-checked:bg-blue-500
-                                    peer-checked:border-blue-500 hover:text-gray-600
-                                    peer-checked:text-gray-600 hover:bg-blue-500
-                                 ">
-                                <div class="flex h-full items-center justify-center p-0 m-0 ">
+                                class="items-center justify-between hidden w-full text-gray-500 bg-gray-200 border-2 border-gray-200 rounded-lg cursor-pointer peer-checked:inline-flex peer-checked:bg-blue-500 peer-checked:border-blue-500 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-blue-500 ">
+                                <div class="flex items-center justify-center h-full p-0 m-0 ">
                                     <svg class="w-12 h-12 p-2 text-white" viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path id="Vector"
@@ -103,20 +94,20 @@
         <x-slot name="title">Pagar</x-slot>
         <x-slot name="content">
             <form wire:submit="store">
-                <div class="grid gap-2 mb-1 grid-cols-2 sm:gap-4 sm:mb-5">
+                <div class="grid grid-cols-2 gap-2 mb-1 sm:gap-4 sm:mb-5">
                     <div class="col-span-full">
                         <label for="title">*Descrição</label>
                         <input
-                            class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900"="Motivo"
+                            class="w-full rounded-md focus:ring focus:ri dark:border-gray-700 dark:text-gray-900"="Motivo"
                             placeholder="Descrição" wire:model="title" required>
                         @error('title')
                             <span class="error">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col-span-full sm:col-span-1 text-left">
+                    <div class="text-left col-span-full sm:col-span-1">
                         <label for="form_payment">*Forma de pagamento</label>
                         <Select wire:model="form_payment" required
-                            class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900">
+                            class="w-full rounded-md focus:ring focus:ri dark:border-gray-700 dark:text-gray-900">
                             <option value=''>Selecione...</option>
                             <option value='DIN'>Dinheiro</option>
                             <option value='CAR'>Cartões</option>
@@ -130,7 +121,7 @@
                     <div class="col-span-1 text-left">
                         <label for="received">*Gerar recibo</label>
                         <Select wire:model="received" required
-                            class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900">
+                            class="w-full rounded-md focus:ring focus:ri dark:border-gray-700 dark:text-gray-900">
                             <option value=''>Selecione...</option>
                             <option value='1'>SIM</option>
                             <option value='2'>NÃO</option>
@@ -150,8 +141,7 @@
                     <div class="col-span-full sm:col-span-1">
                         <label for="value">*Valor total</label>
                         <input
-                            class="w-full rounded-md focus:ring focus:ri
-                    focus:ri dark:border-gray-700 dark:text-gray-900"
+                            class="w-full rounded-md focus:ring focus:ri dark:border-gray-700 dark:text-gray-900"
                             x-mask:dynamic="$money($input, ',')" placeholder="Valor" wire:model="value" required>
                     </div>
                 </div>
@@ -176,18 +166,17 @@
         <x-slot name="title">Editar</x-slot>
         <x-slot name="content">
             <form wire:submit="update">
-                <div class="grid gap-2 mb-1 grid-cols-2 sm:gap-4 sm:mb-5">
+                <div class="grid grid-cols-2 gap-2 mb-1 sm:gap-4 sm:mb-5">
                     <div class="col-span-full sm:col-span-1">
                         <label for="value">*Valor do mês de: {{ $label }}</label>
                         <input
-                            class="w-full rounded-md focus:ring focus:ri
-                        focus:ri dark:border-gray-700 dark:text-gray-900"
+                            class="w-full rounded-md focus:ring focus:ri dark:border-gray-700 dark:text-gray-900"
                             x-mask:dynamic="$money($input, ',')" placeholder="Valor" wire:model="value" required>
                     </div>
                     <div class="col-span-full sm:col-span-1">
                         <label for="status">*Liberar pagamento mês: {{ $label }}</label>
                         <Select wire:model="status" required
-                            class="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900">
+                            class="w-full rounded-md focus:ring focus:ri dark:border-gray-700 dark:text-gray-900">
                             <option value='2'>SIM</option>
                             <option value='0'>NÃO</option>
                         </Select>
@@ -219,22 +208,22 @@
                     <div class="col-span-2 ">
                         <label for="title" class="block text-sm font-medium text-gray-900 dark:text-white">
                             Mês / Ano da mensalidade</label>
-                        <div class="flex w-full items-start justify-center p-3 text-gray-900">
-                            <select class="rounded-l-md w-1/2 " wire:model="mounth">
-                                <option value="1">Janeiro</option>
-                                <option value="2">Fevereiro</option>
-                                <option value="3">Março</option>
-                                <option value="4">Abril</option>
-                                <option value="5">Maio</option>
-                                <option value="6">Junho</option>
-                                <option value="7">Julho</option>
-                                <option value="8">Agosto</option>
-                                <option value="9">Setembro</option>
+                        <div class="flex items-start justify-center w-full p-3 text-gray-900">
+                            <select class="w-1/2 rounded-l-md " wire:model="mounth">
+                                <option value="01">Janeiro</option>
+                                <option value="02">Fevereiro</option>
+                                <option value="03">Março</option>
+                                <option value="04">Abril</option>
+                                <option value="05">Maio</option>
+                                <option value="06">Junho</option>
+                                <option value="07">Julho</option>
+                                <option value="08">Agosto</option>
+                                <option value="09">Setembro</option>
                                 <option value="10">Outubro</option>
                                 <option value="11">Novembro</option>
                                 <option value="12">Dezembro</option>
                             </select>
-                            <select class="rounded-r-md w-1/2 " wire:model="year">
+                            <select class="w-1/2 rounded-r-md " wire:model="year">
                                 @for ($i = 2017; $i <= date('Y') + 1; $i++)
                                     <option value="{{ $i }}" {{ date('Y') == $i ? 'selected' : '' }}>
                                         {{ $i }}
