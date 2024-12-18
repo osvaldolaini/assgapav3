021be5d0f280c2a6de7ca0b7d12a73d91528a729
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-12-18 13:11:25 -0300

    Dados adicionais no info da locação

--- app/Livewire/Admin/Locations/Locations.php
+++ app/Livewire/Admin/Locations/Locations.php
@@ -122,9 +122,8 @@ class Locations extends Component
         $this->showModalView = true;
         if (isset($id)) {
             $data = Location::where('id', $id)->first();
-            dd($data->reason->title);
             $this->detail = [
-                'Motivo'            => $data->reason_event_id ? $data->reason->title : $data->event_type,
+                'Motivo da locação' => $data->reason_event_id ? $data->reason->title : $data->event_type,
                 'Beneficiado'       => $data->event_benefited,
                 'Criada'            => $data->created,
                 'Criada por'        => $data->created_by,
