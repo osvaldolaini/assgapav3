ab2f5848fcf91abe7248e572b05753680639385c
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-01-20 22:53:43 -0300

    Mensalidade atrasada

--- resources/views/livewire/admin/locations/location-new.blade.php
+++ resources/views/livewire/admin/locations/location-new.blade.php
@@ -288,10 +288,13 @@
                                                         <div class="text-sm opacity-50">{{ $item->cpf }} </div>
                                                         @if ($item->discount == 0)
                                                             @if ($item->lateMonthly())
-                                                                <div style="background-color:red;"
-                                                                    class="gap-2 mx-1 text-xs badge badge-error flex-warp ">
-                                                                    {{ $data->category }}
-                                                                </div>
+                                                                <a href="{{ route('monthlys', $item->id) }}"
+                                                                    class="flex px-3 py-2 transition-colors duration-200 hover:text-white dark:hover:bg-blue-500 hover:hover:bg-blue-500 whitespace-nowrap">
+                                                                    <div style="background-color:red;"
+                                                                        class="gap-2 mx-1 text-xs badge badge-error flex-warp ">
+                                                                        {{ $data->category }}
+                                                                    </div>
+                                                                </a>
                                                             @endif
                                                         @endif
                                                     </div>
