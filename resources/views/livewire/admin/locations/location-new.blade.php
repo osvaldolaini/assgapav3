1e2977e07b69f74ef2ca6d376badf194c57f69b2
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-01-20 22:56:19 -0300

    Mensalidade atrasada

--- resources/views/livewire/admin/locations/location-new.blade.php
+++ resources/views/livewire/admin/locations/location-new.blade.php
@@ -289,7 +289,7 @@
                                                         @if ($item->discount == 0)
                                                             @if ($item->lateMonthly())
                                                                 <a href="{{ route('monthlys', $item->id) }}"
-                                                                    class="flex px-3 py-2 transition-colors duration-200 hover:text-white dark:hover:bg-blue-500 hover:hover:bg-blue-500 whitespace-nowrap">
+                                                                    class="flex px-3 py-2 transition-colors duration-200 whitespace-nowrap">
                                                                     <div style="background-color:red;"
                                                                         class="gap-2 mx-1 text-xs badge badge-error flex-warp ">
                                                                         Mensalidades em atraso
