75e1d1614bdfcff7afe42a3a63b331d2f9c027c1
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-05-16 16:48:53 -0300

    master

--- app/Livewire/Admin/Dashboard/DailyReport.php
+++ app/Livewire/Admin/Dashboard/DailyReport.php
@@ -26,97 +26,102 @@ class DailyReport extends Component
     }
     public function getDaily()
     {
-        $d=implode("-",array_reverse(explode("/",$this->search)));
+        $d = implode("-", array_reverse(explode("/", $this->search)));
 
         /*Valor de ajusta do sistema antigo */
-        $adjustmentSystem=4825.06;
+        $adjustmentSystem = 4825.06;
         $config = Configs::find(1);
 
-        $cashiers = Cashier::where('active',1)->where('status',1)->get();
-        $oldCashiers=0;$dayCashiers=0;
-        foreach ($cashiers  as $cashier ) {
-            $c_paid_in=implode("-",array_reverse(explode("/",$cashier->paid_in)));
-            if(strtotime($c_paid_in) < strtotime($d)){
+        $cashiers = Cashier::where('active', 1)->where('status', 1)->get();
+        $oldCashiers = 0;
+        $dayCashiers = 0;
+        foreach ($cashiers  as $cashier) {
+            $c_paid_in = implode("-", array_reverse(explode("/", $cashier->paid_in)));
+            if (strtotime($c_paid_in) < strtotime($d)) {
                 /*Recolhimentos de caixa anteriores ao dia solicitado*/
                 $oldCashiers += $cashier->convert_value($cashier->value);
-            }elseif(strtotime($c_paid_in) == strtotime($d)){
+            } elseif (strtotime($c_paid_in) == strtotime($d)) {
                 $dayCashiers += $cashier->convert_value($cashier->value);
             }
         }
 
 
-        $itens=0;
+        $itens = 0;
         /*Entradas */
-        $receiveds = Received::where('active',1)->get();
+        $receiveds = Received::where('active', 1)->get();
 
-        $listReceiveds=array();$oldReceiveds=0;$dayReceiveds=0;
+        $listReceiveds = array();
+        $oldReceiveds = 0;
+        $dayReceiveds = 0;
         foreach ($receiveds as $received) {
-            $paid_in=implode("-",array_reverse(explode("/",$received->paid_in)));
-            if(strtotime($paid_in) < strtotime($d)){
+            $paid_in = implode("-", array_reverse(explode("/", $received->paid_in)));
+            if (strtotime($paid_in) < strtotime($d)) {
                 /*Entradas anteriores ao dia solicitado*/
-                if($received->form_payment=='DIN'){
+                if ($received->form_payment == 'DIN') {
                     $oldReceiveds += $received->convert_value($received->value);
                 }
-
-            }elseif(strtotime($paid_in) == strtotime($d)){
+            } elseif (strtotime($paid_in) == strtotime($d)) {
                 /*Entradas do dia solicitado*/
-                $itens+=1;
-                if(isset($received->location_id)){
-                    $location = Location::where('id',$received->location_id)->first();
+                $itens += 1;
+                if (isset($received->location_id)) {
+                    $location = Location::where('id', $received->location_id)->first();
                     $tenant = $location->ambienceTenants->title;
-                }else{
+                } else {
                     $tenant = '';
                 }
-                if($received->form_payment=='DIN'){
+                if ($received->form_payment == 'DIN') {
                     $dayReceiveds += $received->convert_value($received->value);
                 }
-                if($received->location_id){
+                if ($received->location_id) {
                     $contact = str_pad($received->location->id, 5, '0', STR_PAD_LEFT);
-                    $text = " - ".$received->ambiences->title .'  ( Contrato nº '.$contact.' )';
-                }else{
+                    $text = " - " . $received->ambiences->title . '  ( Contrato nº ' . $contact . ' )';
+                } else {
                     $text = '';
                 }
-                $listReceiveds[]=[
+                $listReceiveds[] = [
                     'item'          => str_pad($itens, 3, '0', STR_PAD_LEFT),
                     'tenant'        => $tenant,
-                    'title'         => $received->title.$text,
-                    'receipts'      => 'ASSG'.str_pad($received->id, 5, '0', STR_PAD_LEFT).'RC',
-                    'enter'         => 'R$ '. number_format($received->convert_value($received->value),2,",","."),
+                    'title'         => $received->title . $text,
+                    'receipts'      => 'ASSG' . str_pad($received->id, 5, '0', STR_PAD_LEFT) . 'RC',
+                    'enter'         => 'R$ ' . number_format($received->convert_value($received->value), 2, ",", "."),
                     'value'         => $received->convert_value($received->value),
                     'exit'          => '',
                     'form_payment'  => $received->form_payment,
                     'payment'       => $received->payment,
                     'paymentCategory' => $received->paymentCategory,
                 ];
+                dd($listReceiveds);
             }
         }
 
         /*Saídas */
-        $bills = Bill::where('active',1)->where('type','!=','FIN')->get();
+        $bills = Bill::where('active', 1)->where('type', '!=', 'FIN')->get();
 
-        $listBill=array();$oldBills=0;$dayBills=0;
+        $listBill = array();
+        $oldBills = 0;
+        $dayBills = 0;
         foreach ($bills as $bill) {
-            $b_paid_in=implode("-",array_reverse(explode("/",$bill->paid_in)));
-            if(strtotime($b_paid_in) < strtotime($d)){
+            $b_paid_in = implode("-", array_reverse(explode("/", $bill->paid_in)));
+            if (strtotime($b_paid_in) < strtotime($d)) {
                 /*Saídas anteriores ao dia solicitado*/
                 $oldBills += $bill->convert_value($bill->value);
-            }elseif(strtotime($b_paid_in) == strtotime($d)){
+            } elseif (strtotime($b_paid_in) == strtotime($d)) {
                 /*Saídas do dia solicitado*/
-                $itens+=1;
-                if(isset($bill->creditors)){
-                    $creditor =" ( ".$bill->creditors->name." )";
-                }else{
+                $itens += 1;
+                if (isset($bill->creditors)) {
+                    $creditor = " ( " . $bill->creditors->name . " )";
+                } else {
                     $creditor = '';
                 }
 
-                $dayBills+=$bill->convert_value($bill->value);
-                $listBill[]=[
+                $dayBills += $bill->convert_value($bill->value);
+                $listBill[] = [
                     'item'      => str_pad($itens, 3, '0', STR_PAD_LEFT),
                     'tenant'    => '',
-                    'title'     => $bill->title.$creditor,
-                    'receipts'  => 'ASSG'.str_pad($bill->id, 5, '0', STR_PAD_LEFT).'PG',
+                    'title'     => $bill->title . $creditor,
+                    'receipts'  => 'ASSG' . str_pad($bill->id, 5, '0', STR_PAD_LEFT) . 'PG',
                     'enter'     => '',
-                    'exit'      => 'R$ '. number_format($bill->convert_value($bill->value),2,",","."),
+                    'exit'      => 'R$ ' . number_format($bill->convert_value($bill->value), 2, ",", "."),
                 ];
             }
         }
@@ -141,22 +146,24 @@ class DailyReport extends Component
         $today = Carbon::parse(now())->locale('pt-BR');
         // Renderize a view do Livewire
 
-        $html = view('livewire.admin.reports.daily',
-        [
-            'title_postfix' => 'Relatório diário',
-            'subtext'       => 'MOVIMENTO DE CAIXA DO DIA '. $this->search,
-            'responsible'   => Auth::user()->name,
-            'day'           => $this->search,
-            'config'        => $config,
-            'receiveds'     => $listReceiveds,
-            'dayReceiveds'  => $dayReceiveds,
-            'bills'         => $listBill,
-            'dayBills'      => $dayBills,
-            'dayCashiers'   => $dayCashiers,
-            'dayBalance'    => $dayBalance,
-            'oldBalance'    => $oldBalance,
-            'today'         => $today->translatedFormat('d F Y'),
-        ])->render();
+        $html = view(
+            'livewire.admin.reports.daily',
+            [
+                'title_postfix' => 'Relatório diário',
+                'subtext'       => 'MOVIMENTO DE CAIXA DO DIA ' . $this->search,
+                'responsible'   => Auth::user()->name,
+                'day'           => $this->search,
+                'config'        => $config,
+                'receiveds'     => $listReceiveds,
+                'dayReceiveds'  => $dayReceiveds,
+                'bills'         => $listBill,
+                'dayBills'      => $dayBills,
+                'dayCashiers'   => $dayCashiers,
+                'dayBalance'    => $dayBalance,
+                'oldBalance'    => $oldBalance,
+                'today'         => $today->translatedFormat('d F Y'),
+            ]
+        )->render();
         // Adicione o conteúdo HTML ao PDF
         $mpdf->WriteHTML($html);
         $mpdf->SetHTMLFooter('
