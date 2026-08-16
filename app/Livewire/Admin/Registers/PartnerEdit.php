2acfa11049c51cdbce2e295d048cd87ed30046a7
Osvaldo Laini <osvaldolaini@hotmail.com>
2026-08-16 20:28:04 -0300

    v 3.33 16/08/2026

--- app/Livewire/Admin/Registers/PartnerEdit.php
+++ app/Livewire/Admin/Registers/PartnerEdit.php
@@ -141,7 +141,7 @@ class PartnerEdit extends Component
         $this->id = $partner->id;
         $this->name = $partner->name;
         $this->responsible = $partner->responsible;
-        // $this->responsible_name = $partner->parent->name . ' ( ' . $partner->parent->cpf . ' )';
+        $this->responsible_name = $partner->parent->name . ' ( ' . $partner->parent->cpf . ' )';
         $this->kinship = $partner->kinship;
         $this->image = $partner->image;
         $this->date_of_birth = $partner->date_of_birth;
@@ -230,7 +230,7 @@ class PartnerEdit extends Component
             $this->rules['cpf'] = [
                 'required',
                 'min:11',
-                Rule::unique('partners', 'cpf')->ignore($this->id),
+                // Rule::unique('partners', 'cpf')->ignore($this->id),
             ];
         } else {
             $this->rules['cnpj'] = [
