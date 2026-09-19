adf0ffa60724cd561db0f4a6dec476231ba29172
Osvaldo Laini <osvaldolaini@hotmail.com>
2026-09-19 09:23:03 -0300

    v 3.34 23/08/2026

--- app/Livewire/Admin/Dashboard/StatsCard.php
+++ app/Livewire/Admin/Dashboard/StatsCard.php
@@ -89,7 +89,7 @@ class StatsCard extends Component
         if ($partnerLate) {
             $this->partnerLate = count($this->partnerLate());
 
-            dd($this->partnerLate());
+            // dd($this->partnerLate());
         }
         if ($locations) {
             /**Locações */
